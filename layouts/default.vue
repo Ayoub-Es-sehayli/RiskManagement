9d08b90fbd4633c56b7d538d6fba1d0805f0a874
Ayoub Es-sehayli <ayoub1essehayli@outlook.com>
2021-10-11 22:43:09 +0100

    RMA-3 Created the default layout skeleton
    Navigation Component To be added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,1 +1,61 @@
-<template></template>
+<template>
+  <div class="base">
+    <nuxt-link class="logo" to="/"
+      ><img src="~/assets/logo.png" alt="LOGO"
+    /></nuxt-link>
+    <div class="title">Default Title Second Line</div>
+    <div class="nav"></div>
+    <div class="content">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { getModule } from "vuex-module-decorators";
+import { store } from "@/store/index";
+
+@Component
+export default class Default extends Vue {}
+</script>
+
+<style scoped>
+.base {
+  display: grid;
+  grid-template-columns: 200px 1fr;
+  grid-template-rows: 100px 100px 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "logo title"
+    "logo content"
+    "nav content";
+}
+
+.title {
+  justify-self: center;
+  align-self: center;
+  grid-area: title;
+}
+
+.content {
+  grid-area: content;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  grid-auto-rows: 1fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas: ".";
+}
+
+.logo {
+  grid-area: logo;
+}
+
+.nav {
+  justify-self: stretch;
+  grid-area: nav;
+}
+</style>
